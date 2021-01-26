15a924fe8cf487ad7ebd917f9caf53120cb3d959
Yusiwon <danseryu@naver.com>
2021-01-27 00:56:55 +0900

    [백준] 3003_킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ BOJ/src/백준/p3003_킹퀸룩비숍나이트폰.java
@@ -0,0 +1,28 @@
+package ¹éÁØ;
+
+import java.util.Scanner;
+
+public class p3003_Å·Äı·èºñ¼ó³ªÀÌÆ®Æù {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int[] arr = new int[6];
+		
+		arr[0] = 1;
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 2;
+		arr[4] = 2;
+		arr[5] = 8;
+		
+		int number = 0;
+		for(int i = 0; i < 6; i++) {
+			number = input.nextInt();
+			
+			System.out.print(arr[i] - number + " ");
+		}
+	}
+
+}
