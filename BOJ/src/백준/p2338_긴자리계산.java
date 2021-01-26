876cd5d2e191d2c4cce451683a653f9ca538560b
Yusiwon <danseryu@naver.com>
2021-01-27 00:29:37 +0900

    [백준] 2338_긴자리 계산
    BigInger의 덧셈, 뺄셈, 곱셈을 사용

---
+++ BOJ/src/백준/p2338_긴자리계산.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p2338_긴자리계산 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
+		
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+
+}
