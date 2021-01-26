317523a4881f44ffee50895eba760da6333e1100
Yusiwon <danseryu@naver.com>
2021-01-27 00:11:24 +0900

    [백준] 1271_엄청난부자2
    문제해결법은 간단하나 수의 크기를 long크기를 벗어나게 하였다.
    BigInteger의 존재를 처음 알았다.
    BigInteger은  long의 범위보다 큰 무한정의 크기를 갖는다고 한다.
    BigInteger 이름 = new BigInteger("String형"); 으로 선언
    일반 integer형과 다르게 연산하는 방법이 다르다.

---
+++ BOJ/src/백준/p1271_엄청난부자2.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p1271_엄청난부자2 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		BigInteger money = new BigInteger(str[0]);
+		BigInteger al = new BigInteger(str[1]);
+		
+		
+		System.out.println(money.divide(al));
+		System.out.println(money.remainder(al));
+	}
+
+}
