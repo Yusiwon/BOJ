5b1cbfe90d3c849dad1a0f38772d128242cbb5f0
Yusiwon <danseryu@naver.com>
2021-01-27 00:48:58 +0900

    [백준] 2475_검증수
    2제곱수를 구하는 문제이므로 Math이용하지 않고 같은 수를 두번 곱해줘도 OK

--- BOJ/src/백준/p2475_검증수.java
+++ BOJ/src/백준/p2475_검증수.java
@@ -2,7 +2,7 @@ package 백준;
 
 import java.util.Scanner;
 
-public class p1475_검증수 {
+public class p2475_검증수 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +12,7 @@ public class p1475_검증수 {
 		int sum = 0; int num = 0;
 		for(int i = 0; i < 5; i++) {
 			num = input.nextInt();
-			sum += Math.pow(num, 2);
+			sum += num*num;
 		}
 		System.out.println(sum%10);
 	}
