f82787276a37e37a432fe5520283807795d03ba0
Yusiwon <danseryu@naver.com>
2021-01-27 01:12:22 +0900

    [백준] 10757_큰수 A+B
    BigInteger의 덧셈

---
+++ BOJ/src/백준/p10757_큰수AB.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p10757_큰수AB {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(" ");
+		
+		BigInteger a = new BigInteger(arr[0]);
+		BigInteger b = new BigInteger(arr[1]);
+		
+		System.out.println(a.add(b));
+	}
+
+}
