19930a2aa9987918b0990447d8a6a48f03bc1800
Yusiwon <danseryu@naver.com>
2021-01-28 01:28:06 +0900

    [백준]  8370_Plane

---
+++ BOJ/src/백준/p8370_Plane.java
@@ -0,0 +1,20 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p8370_Plane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int k = input.nextInt();
+		int n1 = input.nextInt();
+		int k1 = input.nextInt();
+		
+		int result = (n*k) + (n1*k1);
+		System.out.println(result);
+	}
+
+}
