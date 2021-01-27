27b88845690c2e512940a64ad36538a195d74a68
Yusiwon <danseryu@naver.com>
2021-01-27 22:30:30 +0900

    [백준] 1284_집주소
    나누기와 나머지를 이용한 간단한 알고리즘

---
+++ BOJ/src/백준/p1284_집주소.java
@@ -0,0 +1,49 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class p1284_집주소 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			int number = input.nextInt();
+			
+			if(number == 0)	break;
+			
+			int size = 0; int i = 0;
+			
+			if(number >= 1000) {
+				size = 5;
+				i = 1000;
+			}
+			else if(number >= 100) {
+				size = 4;
+				i = 100;
+			}
+			else if(number >= 10) {
+				size = 3;
+				i = 10;
+			}
+			else if(number >= 1) {
+				size = 2;
+				i = 1;
+			}
+			
+			for(int j = i; j >= 1; j /= 10) {
+				int now = number/j;
+				number = number % j;
+				
+				if(now == 1)	size += 2;
+				else if(now == 0)	size += 4;
+				else	size += 3;
+			}
+			
+			System.out.println(size);
+		}
+	}
+
+}
