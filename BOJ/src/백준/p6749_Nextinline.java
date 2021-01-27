9a03ae10b3a18b5093e98e1e4a80ca9a720c70c0
Yusiwon <danseryu@naver.com>
2021-01-28 01:22:33 +0900

    [백준] 6749_Next in line

---
+++ BOJ/src/백준/p6749_Nextinline.java
@@ -0,0 +1,19 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p6749_Nextinline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int th = input.nextInt();
+		int se = input.nextInt();
+		
+		int bet = se - th;
+		
+		System.out.println(se + bet);
+	}
+
+}
