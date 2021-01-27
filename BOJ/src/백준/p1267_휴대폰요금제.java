689798d9e67058f4163be2f47dfb06b645f5d6b3
Yusiwon <danseryu@naver.com>
2021-01-27 22:08:36 +0900

    [백준] 1267_휴대폰요금제

---
+++ BOJ/src/백준/p1267_휴대폰요금제.java
@@ -0,0 +1,41 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1267_휴대폰요금제 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		
+		String[] temp = br.readLine().split(" ");
+		int[] arr = new int[count];
+		for(int i = 0; i < count; i++) {
+			arr[i] = Integer.parseInt(temp[i]);
+		}
+		
+		int y = 0; int m = 0;
+		for(int i = 0; i < count; i++) {
+			y += arr[i] / 30 + 1;
+			m += arr[i] / 60 + 1;
+		}
+		
+		int ym = y * 10; int mm = m * 15;
+		if(ym < mm) {
+			System.out.println("Y " + ym);
+		}
+		else if(mm < ym) {
+			System.out.println("M " + mm);
+		}
+		else {
+			System.out.println("Y M " + ym);
+		}
+
+		
+	}
+
+}
