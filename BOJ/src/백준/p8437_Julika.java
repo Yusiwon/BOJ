5dc5d7880003ce3502928c42e9d52dfb03653013
Yusiwon <danseryu@naver.com>
2021-01-28 02:40:06 +0900

    [백준]  8437_Julika
    BigInteger

---
+++ BOJ/src/백준/p8437_Julika.java
@@ -0,0 +1,26 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class p8437_Julika {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		String b = br.readLine();
+		
+		
+		BigInteger Ba = new BigInteger(a);
+		BigInteger Bb = new BigInteger(b);
+		BigInteger two = new BigInteger("2");
+		System.out.println(Ba.add(Bb).divide(two));
+		System.out.println(Ba.subtract(Bb).divide(two));
+	}
+
+}
