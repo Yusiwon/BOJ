4b8a4ef1742ec7c89539255d143ca7428a203c18
Yusiwon <danseryu@naver.com>
2021-01-29 23:48:43 +0900

    [백준] 18108_1998년생인 내가 태국에서는 2541년생?

---
+++ BOJ/src/백준/p18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class p18108_1998년생인내가태국에서는2541년생 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println(n-543);
+	}
+
+}
