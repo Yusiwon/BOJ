24d1a4526662c6ec19dccc2e9657f5e7a958dd00
Yusiwon <danseryu@naver.com>
2021-01-29 23:46:07 +0900

    [백준] 15894_수학은 체육과목입니다

---
+++ BOJ/src/백준/p15894_수학은체욱과목입니다.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p15894_수학은체욱과목입니다 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger("4");
+		System.out.println(a.multiply(b));
+	}
+
+}
