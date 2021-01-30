170c1f5c1ac568d491f31436d55909473f7bdd0b
Yusiwon <danseryu@naver.com>
2021-01-31 00:08:14 +0900

    [백준] 10162_전자레인지
    시간연산
    나누기의 몫값과 나머지값을 이용한 연산

---
+++ BOJ/src/백준/p10162_전자레인지.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10162_전자레인지 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int time = Integer.parseInt(br.readLine());
+
+		if(time % 10 != 0 ) {
+			System.out.println(-1);
+		}
+		else {
+			int a = time / 300;
+
+			int b = time % 300;
+			b /= 60;
+			
+			int c = time % 60;
+			c /= 10;
+			
+			System.out.println(a + " " + b + " " + c);
+		}
+
+	}
+
+}
