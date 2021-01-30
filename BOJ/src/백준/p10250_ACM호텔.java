593498368ac06c4807d0540a5dfb7bc10d61bcff
Yusiwon <danseryu@naver.com>
2021-01-30 22:44:33 +0900

    [백준] 10250_ACM호텔
    몫과 나머지값을 이용하여 푸는 문제
    
    처음에는 단순하게 동과 호 사이에 "0"이 들어가야한다고 생각했는데 호수가 10을 넘어가게 되면 "0"이 필요없다는것을 나중에 알게되었다.
    if~else문을 통해 처리해주었다.
    또한 나머지값을 이용하여 동을 결정해주기때문에 나머지값이 0이 나오게 되면 꼭대기 층이 아닌 0층이 되어버리는 문제가 생겼다.
    이러한 문제도 if~else문을 이용하여 동을 꼭대기층으로 바꾸어주고 동시에 호값에 1을 빼주었다.

---
+++ BOJ/src/백준/p10250_ACM호텔.java
@@ -0,0 +1,44 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10250_ACM호텔 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		int height, width, n, dong, ho;
+		
+		for(int i = 0; i < t; i++) {
+			String[] str = br.readLine().split(" ");
+			height = Integer.parseInt(str[0]);
+			width = Integer.parseInt(str[1]);
+			n = Integer.parseInt(str[2]);
+			
+			dong = n % height;
+			ho = n / height + 1;
+			
+			if(dong == 0) {
+				dong = height;
+				ho--;
+			}
+			
+			if(ho >= 10) {
+				System.out.println(dong + "" + ho);
+			}
+			else {
+				System.out.println(dong + "0" + ho);
+			}
+			
+		}
+		
+		
+
+	}
+
+}
