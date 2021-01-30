68fab6479bd3d0df9be13c4fbb298debc246dee1
Yusiwon <danseryu@naver.com>
2021-01-31 02:34:52 +0900

    [백준]  2609_최대공약수와 최대공배수_유클리드호제법
    유클리드 호제 법으로 최대공약수와 최대공배수를 구했다.
    *최대공약수*
    -> 큰 수 A를 작은수 B로 나누었을때 나누었을때 나누어 떨어진다면 최대공약수는 B가 된다.
    1. 입력 받은 두 수중 큰 수를 A, 작은 수를 B로 정한다.
    2. A를 B로 나눈값의 나머지를 R로 지칭한다.
    3. R이 0이라면 A는 B로 나누어 지기 때문에 최대공약수는 B가 된다.
    4. 만약 R이 0이 아니라면 A값은 B로, B값은 R로 변경한 뒤 3번 과정을 반복한다.
    
    *최소공배수*
    1. 두 수의 최대 공약수를 유클리드 호제법으로 구한다.
    2. 두 수 A, B를 곱한 뒤 최대 공약수로 나눈 값을 최소 공배수로 출력한다.

--- BOJ/src/백준/p2609_최대공약수와최소공배수.java
+++ BOJ/src/백준/p2609_최대공약수와최소공배수.java
@@ -1,10 +1,36 @@
 package 백준;
 
+import java.util.Scanner;
+
 public class p2609_최대공약수와최소공배수 {
 
 	public static void main(String[] args) {
-		// TODO Auto-genated method stub
+		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		long n1 = input.nextInt();
+		long n2 = input.nextInt();
+
+		long cdg = cdg(Math.max(n1, n2), Math.min(n1, n2));
+		System.out.println(cdg);
+		
+		long csg = csg(n1, n2, cdg);
+		System.out.println(csg);
+		
+	}
+	
+	public static long cdg(long a, long b) {
+		while(b > 0) {
+			long temp = a;
+			a = b;
+			b = temp % b;
+		}
+		return a;
+	}
+	
+	public static long csg(long a, long b, long cdg) {
+		long result = (a * b) / cdg;
+		return result;
 	}
 
 }
