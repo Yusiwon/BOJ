a7e98fa1a953ce22fcfd448ad5296ed901542d58
Yusiwon <danseryu@naver.com>
2021-01-30 23:45:36 +0900

    no message

---
+++ BOJ/src/백준/p1373_2진수8진수.java
@@ -0,0 +1,35 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1373_2진수8진수 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		if(str.length() % 3 == 1) {
+			str = "00" + str;
+		}
+		else if(str.length() % 3 == 2) {
+			str = "0" + str;
+		}
+
+		String result = ""; String sample = ""; int n = 0;
+		for(int i = 0; i < str.length(); i += 3) {
+			sample = str.substring(i, i+3);
+			n = (sample.charAt(0) - '0') * 4
+					+ (sample.charAt(1) - '0') * 2
+					+ (sample.charAt(2) - '0');
+			result = result + String.valueOf(n);
+		}
+		
+		System.out.println(result);
+	}
+
+}
+
