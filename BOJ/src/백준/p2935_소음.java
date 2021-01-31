ca793fb3be089f26aca57edcbeb9f1e4f459ba80
Yusiwon <danseryu@naver.com>
2021-01-31 22:58:37 +0900

    [백준] 2935_소음

---
+++ BOJ/src/백준/p2935_소음.java
@@ -0,0 +1,45 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2935_소음 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = br.readLine();
+		String op = br.readLine();
+		String b = br.readLine();
+		
+		if(a.length() < b.length()) {
+			String temp = a;
+			a = b; b = temp;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(a);
+		
+		if(op.equals("*")) {
+			for(int i = 0; i < b.length()-1; i++) {
+				sb.append("0");
+			}
+		}
+		else {
+			if(a.length() == b.length()) {
+				sb.setCharAt(0, '2');
+			}
+			else {
+				sb.setCharAt(a.length() - b.length(), '1');
+			}
+		}
+		System.out.println(sb.toString());
+		
+
+	}
+
+}
