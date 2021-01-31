c2c137cea2fe5b259d5af9864aa2762c8c3d59d2
Yusiwon <danseryu@naver.com>
2021-02-01 00:03:44 +0900

    [백준] 17413_단어 뒤집기2
    단어를 뒤집어 출력하는 문제로 처음에는 for문의 index를 뒤집어 출력하려 하였으나 문제가 발생
    stack의 FILO의 특징을 이용하여 해결하였다.

---
+++ BOJ/src/백준/p17413_단어뒤집기2.java
@@ -0,0 +1,58 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class p17413_단어뒤집기2 {
+
+	static Stack<Character> stack;
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		stack = new Stack<>();
+		char ch;	boolean bool = false;
+		for(int i = 0; i < str.length(); i++) {
+			ch = str.charAt(i);
+
+			if(bool) {
+				if(ch == '>') {
+					bool = false;
+				}
+				
+				System.out.print(ch);
+			}
+			else {
+				if(ch == ' ') {
+					printS();
+					System.out.print(" ");
+				}	
+				else if(ch == '<') {
+					if(stack.size() != 0) {
+						printS();	
+					}
+					System.out.print(ch);
+					bool = true;
+				}
+				else {
+					stack.push(ch);
+				}
+			}
+		}
+		printS();
+
+
+	}
+
+	public static void printS() {
+		int size = stack.size();
+		for(int i = 0; i < size; i++) {
+			System.out.print(stack.pop());
+		}
+	}
+
+}
