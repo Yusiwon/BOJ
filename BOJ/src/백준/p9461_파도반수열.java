ad7a666854a0cd860a4ac77bb7a3746c3064bdcd
Yusiwon <danseryu@naver.com>
2021-02-07 23:36:16 +0900

    [백준] 9461_파도반 수열
    재귀함수를 이용한 알고리즘 풀이
    하지만 파도반수열에서는 1번 index 와 2번 index의 합이 3번 index가 아닌 4번 index로 간다는 점이 특징!

---
+++ BOJ/src/백준/p9461_파도반수열.java
@@ -0,0 +1,36 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p9461_파도반수열 {
+
+	static long[] arr = new long[101];
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		padovan();
+		
+		for(int i = 0; i < test; i++) {
+			int b = Integer.parseInt(br.readLine());
+			System.out.println(arr[b]);
+		}
+
+	}
+	
+	public static void padovan() {
+		arr[1] = 1;
+		arr[2] = 1;
+		arr[3] = 1;
+		
+		for(int i = 4; i < 101; i++) {
+			arr[i] = arr[i-2] + arr[i-3];
+		}
+	}
+
+}
