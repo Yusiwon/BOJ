dc67073a6b37449d5a6590fd9cf5607ae01d6358
Yusiwon <danseryu@naver.com>
2021-02-08 22:52:41 +0900

    [백준]  14928_큰 수

---
+++ BOJ/src/백준/p14928_큰수.java
@@ -0,0 +1,23 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p14928_큰수 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		long remain = 0;
+		for(int i = 0; i < n.length(); i++) {
+			remain = (remain * 10 + (n.charAt(i) - '0')) % 20000303;
+		}
+		System.out.println(remain);
+	}
+
+}
