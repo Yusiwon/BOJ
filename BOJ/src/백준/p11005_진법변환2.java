a7f44e710d7e195822f1b605e6de97155aadc23b
Yusiwon <danseryu@naver.com>
2021-02-08 21:55:42 +0900

    [백준] 11005_진법 변환2

---
+++ BOJ/src/백준/p11005_진법변환2.java
@@ -0,0 +1,34 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p11005_진법변환2 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int n = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(n > 0) {
+			int remain = n % b;
+			if(remain < 10) {
+				sb.append((char)(remain + '0'));
+			}
+			else {
+				sb.append((char)(remain - 10 + 'A'));
+			}
+			n /= b;
+		}
+		
+		System.out.println(sb.toString());
+
+	}
+
+}
