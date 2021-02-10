d200522a33837cd42a5ad4b8fc2426b8b15d8d97
Yusiwon <danseryu@naver.com>
2021-02-10 18:43:08 +0900

    [백준] 4101_크냐?

---
+++ BOJ/src/백준/p4101_크냐.java
@@ -0,0 +1,34 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p4101_크냐 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String[] str = br.readLine().split(" ");
+			
+			int[] arr =  new int[2];
+			arr[0] = Integer.parseInt(str[0]);
+			arr[1] = Integer.parseInt(str[1]);
+			
+			if(arr[0] == 0 && arr[1] == 0)	break;
+			
+			if(arr[0] <= arr[1]) {
+				System.out.println("No");
+			}
+			else {
+				System.out.println("Yes");
+			}
+			
+		}
+
+	}
+
+}
