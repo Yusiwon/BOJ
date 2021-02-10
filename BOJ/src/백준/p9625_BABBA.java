c0782f63d465e182bb63b8c58b629be0d1b07383
Yusiwon <danseryu@naver.com>
2021-02-10 22:56:58 +0900

    [백준] 9625_BABBA
    처음에는 문제를 직관적으로 풀려고 했으나 결과값을 순서대로 써보니 규칙을 찾았다
    따라서 피보나치수열을 이용하여 해결하였다.
    
    button : 0 1
    button : 1 1
    button : 1 2
    button : 2 3
    button : 3 5

---
+++ BOJ/src/백준/p9625_BABBA.java
@@ -0,0 +1,32 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p9625_BABBA {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int button = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[45];
+		int[] brr = new int[45];
+		
+		arr[0] = 0;
+		arr[1] = 1;
+		brr[0] = 1;
+		brr[1] = 1;
+		
+		for(int i = 2; i < 45; i++) {
+			arr[i] = arr[i-1] + arr[i-2];
+			brr[i] = brr[i-1] + brr[i-2];
+		}
+		
+		System.out.println(arr[button-1] + " " + brr[button-1]);
+		
+	}
+
+}
