7d33462225478265c1a0eec581e8793842939e9e
Yusiwon <danseryu@naver.com>
2021-02-10 22:26:07 +0900

    [백준] 10214_Baseball

---
+++ BOJ/src/백준/p10214_Baseball.java
@@ -0,0 +1,40 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10214_Baseball {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < test; i++) {
+			int y = 0; int k = 0;
+			for(int j = 0; j < 9; j++) {
+				String[] str = br.readLine().split(" ");
+				int a = Integer.parseInt(str[0]);
+				int b = Integer.parseInt(str[1]);
+				
+				y += a;
+				k += b;
+			}
+			
+			if(y > k) {
+				System.out.println("Yonsei");
+			}
+			else if(y < k) {
+				System.out.println("Korea");
+			}
+			else {
+				System.out.println("Draw");
+			}
+
+		}
+
+	}
+
+}
