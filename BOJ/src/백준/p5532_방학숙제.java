427a326b5b4e47c9786db0cec18d9f41deea3f24
Yusiwon <danseryu@naver.com>
2021-02-10 12:08:17 +0900

    [백준] 5532_방학숙제

---
+++ BOJ/src/백준/p5532_방학숙제.java
@@ -0,0 +1,31 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p5532_방학숙제 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int l = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
+		
+		int kor, mat;
+		kor = a / c;
+		if(a % c != 0)	kor++;
+		
+		mat = b / d;
+		if(b % d != 0)	mat++;
+		
+		System.out.println(l - Math.max(kor,  mat));
+
+	}
+
+}
