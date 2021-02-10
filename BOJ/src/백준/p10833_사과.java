6a16bf265eec264d1e82777371a7df4ccd389850
Yusiwon <danseryu@naver.com>
2021-02-10 11:33:37 +0900

    [백준] 10833_사과

---
+++ BOJ/src/백준/p10833_사과.java
@@ -0,0 +1,25 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10833_사과 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		
+		
+		int result = 0;
+		for(int i = 0; i < count; i++) {
+			String[] str = br.readLine().split(" ");
+			result += Integer.parseInt(str[1]) % Integer.parseInt(str[0]);
+		}
+		System.out.println(result);
+
+	}
+
+}
