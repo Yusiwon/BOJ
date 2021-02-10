83c920831879e9cbe4da7978589998e50ef1095c
Yusiwon <danseryu@naver.com>
2021-02-10 19:44:48 +0900

    [백준] 2406_지능형 기차2

---
+++ BOJ/src/백준/p2406_지능형기차2.java
@@ -0,0 +1,27 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2406_지능형기차2 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int max = 0;
+		for(int i = 0; i < 10; i++) {
+			String[] str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[1]);
+			
+			sum -= a;
+			sum += b;
+			if(sum > max)	max = sum;
+		}
+		System.out.println(max);
+	}
+
+}
