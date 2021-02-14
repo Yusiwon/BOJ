541b69d58ba2e2f6999e8bc1d8a92076d422d492
Yusiwon <danseryu@naver.com>
2021-02-14 14:47:10 +0900

    [백준] 15439_Vera and Outfits

---
+++ BOJ/src/백준/p15439_VeraandOutfits.java
@@ -0,0 +1,16 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p15439_VeraandOutfits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+		System.out.println(n*(n-1));
+	}
+
+}
