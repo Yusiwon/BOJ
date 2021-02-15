e030f1c34f7cc9b64ba8e1345e2ede957a821ccf
Yusiwon <danseryu@naver.com>
2021-02-15 23:17:17 +0900

    [백준] 9316_Hello Judge

---
+++ BOJ/src/백준/p9316_HelloJudge.java
@@ -0,0 +1,18 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class p9316_HelloJudge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+		for(int i = 1; i < n+1; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+	}
+
+}
