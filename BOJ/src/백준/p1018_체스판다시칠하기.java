0c9357f3a07ffbd6b61fc99b5ebd0ad86f98667e
Yusiwon <danseryu@naver.com>
2021-02-17 22:41:35 +0900

    [백준] 1018_체스판 다시 칠하기
    브루트포스 알고리즘
    
    입력에서 주어지는 W와 B를 bool배열에 W-true B-false로 넣어주었다.
    8*8배열을 확인하는것이므로 모든판이 다 바뀌어야된다하면 64번 바뀐다. 즉, 이는 64번이 최대수라는 것이다.
    
    전체 배열에서 8*8크기의 배열을 탐색하려면 (전체길이- 7)번을 하면 전부 탐색할수 있다.
    예를들어 전체 길이가 13이라고 하자면 첫번째탐색때 길이 8에 닿는다 그 다음 탐색을 하면 9에 닿을 것이다.
    8, 9, 10, 11, 12, 13 즉 5번을 탐색하게 되므로 주의해야한다.
    
    4중 for문을 이용하여 탐색을 하게 된다.
    현재위치의 배열에서 경우의 수를 구하게 되는 경우 첫번째 정사각형이 'W'일 경우와 'B'일 경우가 있다.
    이때 각행의 마지막일때는 boolean변수를 바꾸지 않는다.
    WBWB
    BWBW
    해당 예시를 보면 첫번째 행에서 B로 끝나서 다음행에서는 B로 시작하기 때문이다.
    
    각 경우의 수를 계산해주고 이중 최소값을 저장하여 출력해주었다.
    한번은 'W'일 경우의 4중 for문 그리고 'B'일 경우의 4중 for문을 할 수도 있지만 하나의 4중 for문에 해주었다.
    'W'로 시작하는 경우에 맞게 움직이는 boolean변수와 'B'로 시작하는 경우에 맞게 움직이는 boolean변수 각가 만들어 주었다.

---
+++ BOJ/src/백준/p1018_체스판다시칠하기.java
@@ -0,0 +1,69 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1018_체스판다시칠하기 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int row = Integer.parseInt(str[0]);
+		int colum = Integer.parseInt(str[1]);
+
+		boolean[][] bool = new boolean[row][colum];
+
+		for(int i = 0; i < row; i++) {
+			String line = br.readLine();
+
+			for(int j = 0; j < colum; j++) {
+				char ch = line.charAt(j);
+
+				if(ch == 'W') {
+					bool[i][j] = true;
+				}
+				else if(ch == 'B') {
+					bool[i][j] = false;
+				}
+			}
+		}
+
+		int min = 10000;
+		for(int n = 0; n < row-7; n++) {
+			for(int m = 0; m < colum-7; m++) {
+
+				int w = 0; int b = 0;
+				boolean wb = true; boolean bb = false;
+				for(int i = 0; i < 8; i++) {
+					for(int j = 0; j < 8; j++) {
+						if(bool[n+i][m+j] == wb) {
+							b++;
+						}
+						else if(bool[n+i][m+j] == bb){
+							w++;
+						}
+						if(j != 7) {
+							wb = !wb;
+							bb = !bb;
+						}
+
+					}
+				}
+
+				if(min > w) {
+					min = w;
+				}
+				if(min > b) {
+					min = b;
+				}
+			}
+		}
+
+		System.out.println(min);
+
+	}
+
+}
