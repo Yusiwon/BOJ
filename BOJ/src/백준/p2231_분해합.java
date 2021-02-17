79d101ad939f7a9732e7d377f630fe64d5e5c26a
Yusiwon <danseryu@naver.com>
2021-02-17 19:50:52 +0900

    [백준] 2231_분해합
    브루트포스

---
+++ BOJ/src/백준/p2231_분해합.java
@@ -0,0 +1,35 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2231_분해합 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine());
+		
+		int index = 0;
+		for(int i = 1; i < number; i++) {
+			int now = i;
+			int sum = i;
+			
+			while(now != 0) {
+				sum += now % 10;
+				now /= 10;
+			}
+			
+			if(sum == number) {
+				index = i;
+				break;
+			}
+		}
+		
+		
+		System.out.println(index);
+	}
+
+}
