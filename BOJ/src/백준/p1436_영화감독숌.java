94c7141368a0fc9e03b43e052d5db2b46d0a2d12
Yusiwon <danseryu@naver.com>
2021-02-17 23:11:07 +0900

    [백준] 1436_영화감독 숌
    브루트포스알고리즘
    숫자를 하나씩 올려서 해당 숫자에 666을 포함하고 있는지 확인후 포함하면 count를 해주었다.
    String의 contains는 해당 문자열의 포함여부에 따라 true, false를 반환하므로 이를 이용하였다.
    
    처음에는 "666"이 포함하는지 확인 후 숫자를 늘려갔었다.
    하지만 count숫자가 n과 같을 경우를 while문시작에서 판별해주므로 while문의 시작으로 돌아오기전에 숫자를 하나 올려주므로 원하는 값을 얻을 수 없다.
    이를 해결하기 위해서는 숫자를 먼저 올려주고 count를 해주어야 하는데 이때 시작 숫자를 666으로 해주었기때문에 while문은 667부터 탐색을 하게 되므로 666일때의 count를 하나 해주기때문에
    보통count를 0부터 시작하는 반면 이번에는 1부터 시작하게 되었다.

--- BOJ/src/백준/p1436_영화감독숌.java
+++ BOJ/src/백준/p1436_영화감독숌.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class p1436_¿µÈ­°¨µ¶¼ò {
 
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
+
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
