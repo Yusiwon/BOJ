6fff309d13b9738a06afe99f7fd30ea74d766850
Yusiwon <danseryu@naver.com>
2021-02-18 21:52:28 +0900

    [백준] 9020_골드바흐의 추측
    에라토스테네스의 체
    
    에라토스테네스의 체 알고리즘을 이용하여 입력값들중 소수인것을 모두 골라낸 후에 입력을 받아냈다.
    처음에는 입력을 받을때마다 입력값보다 작은 소수값들을 구한뒤 연산을 하려했으나 이렇게 된다면 작은수의 소수들을 처음에 이미 소수인것을 구했으나 다시 같은 연산을 반복하게 된다.
    
    소수를 다 구했다면, 소수들의 합을 구해야하는데 두 소수간의 차이가 가장 적으려면 입력값이 짝수이므로 2로 나눈값의 합이 가장 차이가 적을것이다.
    6의 경우 3, 3으로 둘다 소수이므로 이미 구해놓은 소수배열에서 소수인지 판단한 후 소수임을 확인하면 끝이지만
    8의 경우 4, 4로 두 수가 소수가 아니다.
    두 수중 하나에 1을 빼게된다면 다른 수에는 1을 더해야 합은 같아진다.
    따라서 왼쪽으로 이동하는 변수(-)와 오른쪽으로 이동하는 변수(+)를 선언하여 while문을 통해 소수일때까지 이동하였다.
    두 변수 모두 소수에 다다르게 되면 그 값들을 출력하여 끝내게 된다.

---
+++ BOJ/src/백준/p9020_골드바흐의추측.java
@@ -0,0 +1,46 @@
+package πÈ¡ÿ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p9020_∞ÒµÂπŸ»Â¿«√ﬂ√¯ {
+
+	static boolean[] prime = new boolean[10001];
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		getPrime();
+		
+		for(int i = 0; i < n; i++) {
+			int now = Integer.parseInt(br.readLine());
+			int left = now / 2;
+			int right = now / 2;
+			
+			while(prime[left] || prime[right]) {
+				left--;
+				right++;
+			}
+			
+			System.out.println(left + " " + right);
+		}
+
+	}
+	
+	public static void getPrime() {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i = 2; i*i < prime.length; i++) {
+			if(!prime[i]) {
+				for(int j = i*i; j < prime.length; j += i) {
+					prime[j] = true;
+				}
+			}
+		}
+	}
+
+}
