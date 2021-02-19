9c8ea330dbb82ee4ed3d638a25e176d445b00646
Yusiwon <danseryu@naver.com>
2021-02-19 23:47:15 +0900

    [백준] 3009_네번째점_미완성

---
+++ BOJ_I/src/BOJ/p3009_네번째점.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p3009_네번째점 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
