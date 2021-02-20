495fd92a37df9f820b71b8cdb46bbf58b16b3963
Yusiwon <danseryu@naver.com>
2021-02-20 14:03:04 +0900

    [백준] 15651_N과 M(3)
    백트래킹
    N과 M(1)과 N과 M(2)의 공통조건은 앞에 사용된 숫자는 사용하지 않는것이었다.
    따라서 boolean타입의 배열을 만들어 앞에서의 사용여부를 확인하여 사용안햇을때만 재귀를 하게 하였었다.
    그러나 이번문제에는 숫자중복이 가능하므로 이전의 사용여부는 확인하지 않아도 된다.
    즉, boolean타입의 배열의 필요성이 없다.
    또한 for문을 돌때마다 false(방문X)인지를 확인했었는데 이 또한 필요없다.
    해당 조건만 없애주면 해결된다.

---
+++ BOJ_I/src/BOJ/p15651_N과M3.java
@@ -0,0 +1,41 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p15651_N°úM3 {
+
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int n = Integer.parseInt(str[0]);
+        int size = Integer.parseInt(str[1]);
+
+        arr = new int[size];
+
+        dfs(n, size, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int n, int size, int depth) {
+        if(depth == size) {
+            for(int i = 0; i < size; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            arr[depth] = i+1;
+            dfs(n, size, depth+1);
+        }
+    }
+}
