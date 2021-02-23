b4fbc38ee4a55918a270db9360a0612efe4f5eb2
Yusiwon <danseryu@naver.com>
2021-02-23 13:02:10 +0900

    [백준] 15654_N과 M(5) https://www.acmicpc.net/problem/15654
    백트래킹
    기존 N과 M시리즈와는 입력의 형태가 다르다
    이번 문제에서는 문제를 풀기위한 숫자도 주어진다.
    이 숫자들을 배열에 저장하고 출력값이 사전순으로 증가하는 순이므로 dfs전에 배열정렬을 해준 후 dfs를 실행한다.
    결과값을 저장하는배열에 숫자를 저장하는것이 아닌 배열의 숫자를 저장해주는점말고는 dfs의 역할은 그대로이다.

---
+++ BOJ_I/src/BOJ/p15654_N과M5.java
@@ -0,0 +1,54 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class p15654_N°úM5 {
+
+    static int[] arr;
+    static int[] temp;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int size = Integer.parseInt(str[1]);
+
+        arr = new int[size];
+        visit = new boolean[n];
+
+        temp = new int[n];
+        String[] tp = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            temp[i] = Integer.parseInt(tp[i]);
+        }
+
+        Arrays.sort(temp);
+        dfs(n, size, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int n, int size, int depth) {
+        if (depth == size) {
+            for (int i = 0; i < size; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = temp[i];
+                dfs(n, size, depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
