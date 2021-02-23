6268c58f487d0e8488bf32dee85c3f0d2c24e6d5
Yusiwon <danseryu@naver.com>
2021-02-23 23:02:10 +0900

    [백준] 15665_N과 M(11)_미해결(시간초과) https://www.acmicpc.net/problem/15665
    백트래킹
    문제해결은 됐으나 시간초과를 아직 해결하지 못했다.

---
+++ BOJ_I/src/BOJ/p15665_N과M11.java
@@ -0,0 +1,55 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class p15665_N°úM11 {
+
+    static int[] arr;
+    static int[] temp;
+    static StringBuilder sb = new StringBuilder();
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int size = Integer.parseInt(str[1]);
+
+        arr = new int[size];
+        temp = new int[n];
+
+        String[] tp = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            temp[i] = Integer.parseInt(tp[i]);
+        }
+        Arrays.sort(temp);
+        dfs(n, size, 0);
+
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+
+    public static void dfs(int n, int size, int depth) {
+        if (depth == size) {
+            for (int i = 0; i < size; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            if (!list.contains(sb.toString())) {
+                list.add(sb.toString());
+            }
+            sb.delete(0, sb.length());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = temp[i];
+            dfs(n, size, depth + 1);
+        }
+    }
+}
