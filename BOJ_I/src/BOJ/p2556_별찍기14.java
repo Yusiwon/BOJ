eb5abe472fdb60d6090329832f886092bfa468a0
Yusiwon <danseryu@naver.com>
2021-02-25 22:11:12 +0900

    [백준] 2556_별찍기14 https://www.acmicpc.net/problem/2556
    예전에 해둔 코드를 찾았다

---
+++ BOJ_I/src/BOJ/p2556_별찍기14.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class p2556_º°Âï±â14{
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
